4e71b43fb9708b88553abf3ebb852960f383e5b5
anggitaaaaw <affebriany@gmail.com>
2020-02-18 10:07:11 +0700

    rev ven dev

--- application/models/Labelmodel.php
+++ application/models/Labelmodel.php
@@ -398,9 +398,9 @@ class Labelmodel extends CI_Model {
     function view_vendor_delivery(){
         $this->db->select('*');
         $this->db->from('trx_deliverynote'); 
-        $this->db->join('trx_ven_delivery', 'trx_ven_delivery.batch_no = trx_deliverynote.serial_id');
+        $this->db->join('trx_ven_receive', 'trx_ven_receive.batch_no = trx_deliverynote.serial_id');
         $this->db->join('m_vendor_set', 'm_vendor_set.item_no = trx_deliverynote.item_code');
-        $this->db->group_by('trx_ven_delivery.dn_no');
+        $this->db->group_by('trx_ven_receive.dn_no');
 
         return $this->db->get();
     }
--- javascript_data/vendor_delivery.js
+++ javascript_data/vendor_delivery.js
@@ -39,10 +39,16 @@ function format ( d ) {
                                 '<td>'+dataa[i].qty_actual+'</td>'+
                                 '<td>'+dataa[i].weight_actual+'</td>'+
                                 '<td>'+dataa[i].qty_balance+'</td>'+
-                                '<td>'+dataa[i].weight_balance+'</td>'+
-                                '<td><button data-toggle="modal" data-target="#editQty" class="btn btn-danger    mr-2" value='+dataa[i].batch_no+' onclick="edit_qty(this.value)"><i class="ik ik-edit"></i>Edit Qty</button></td>'+
-                                '<td><button data-toggle="modal" data-target="#modal_printlabel" class="btn btn-warning mr-2" value='+dataa[i].spk_no+' onclick="print_label(this.value)"><i class="ik ik-printer"></i>Print</button></td>'+
-                                '<td>Open</td>'+
+                                '<td>'+dataa[i].weight_balance+'</td>';
+                                if(dataa[i].status_dn == 'open'){
+                                    table += '<td><button data-toggle="modal" data-target="#editQty" class="btn btn-danger    mr-2" value='+dataa[i].batch_no+' onclick="edit_qty(this.value)" disabled><i class="ik ik-edit"></i>Edit Qty</button></td>'+
+                                    '<td><button data-toggle="modal" data-target="#modal_printlabel" class="btn btn-warning mr-2" value='+dataa[i].spk_no+' onclick="print_label(this.value)" disabled><i class="ik ik-printer"></i>Print</button></td>';
+                                }else{
+                                    table += '<td><button data-toggle="modal" data-target="#editQty" class="btn btn-danger    mr-2" value='+dataa[i].batch_no+' onclick="edit_qty(this.value)"><i class="ik ik-edit"></i>Edit Qty</button></td>'+
+                                    '<td><button data-toggle="modal" data-target="#modal_printlabel" class="btn btn-warning mr-2" value='+dataa[i].spk_no+' onclick="print_label(this.value)"><i class="ik ik-printer"></i>Print</button></td>';
+                                }
+                               
+                                table += '<td>'+dataa[i].status_dn+'</td>'+
                             '</tr>';
                     }
                     $('#det_batch'+d.dn_no).html(table);
