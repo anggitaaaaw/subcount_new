2c2dabf085b3ddfe12440673f8f4bfc7ce1a6e40
Hilman Fadillah <hilmanf11@gmail.com>
2020-03-18 16:09:20 +0700

    update

--- application/views/page/making_label/report_receiving_subcont.php
+++ application/views/page/making_label/report_receiving_subcont.php
@@ -32,6 +32,137 @@
                                             <div class="card" style="overflow: auto; min-height: 484px;">
                                                 <div class="card-body form-group row">
                                                     <div class="col-sm-12">
+                                                        <div class="row">
+                                                            <div class="col-md-6">
+                                                                <div class="col-md-12">
+                                                                    <div class="form-group row" style="margin: 1px;">
+                                                                        <label class="col-sm-4 col-form-label">Trans Date</label>
+                                                                        <div class="col-sm-3">
+                                                                            <input type="text" placeholder="From" class="form-control datetimepicker-input" id="datepicker" data-toggle="datetimepicker" data-target="#datepicker" onchange="date_to()">
+                                                                        </div>
+                                                                        <div class="col-sm-3">
+                                                                            <input type="text" placeholder="To" class="form-control datetimepicker-input" id="datepicker2" data-toggle="datetimepicker" data-target="#datepicker2" onchange="date_from(this.value)">
+                                                                        </div>
+                                                                        <div class="col-sm-2">
+                                                                            <div class="checkbox-fade fade-in-primary">
+                                                                                <label>
+                                                                                    <input type="checkbox" value="" id="check_date" checked onchange="check_date()">
+                                                                                    <span class="cr">
+                                                                                        <i class="cr-icon ik ik-check txt-primary"></i>
+                                                                                    </span>
+                                                                                    <span>ALL</span>
+                                                                                </label>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="form-group row" style="margin: 1px;">
+                                                                        <label class="col-sm-4 col-form-label">Subcont Name</label>
+                                                                        <div class="col-sm-6">
+                                                                            <select type="text" id="subcount_name_report" class="form-control select2" onchange="select_subcount(this.value)"></select>
+                                                                        </div>
+                                                                        <div class="col-sm-2">
+                                                                            <div class="checkbox-fade fade-in-primary">
+                                                                                <label>
+                                                                                    <input type="checkbox" value="" id="check_subcount" checked onchange="check_subcount()">
+                                                                                    <span class="cr">
+                                                                                        <i class="cr-icon ik ik-check txt-primary"></i>
+                                                                                    </span>
+                                                                                    <span>ALL</span>
+                                                                                </label>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="form-group row" style="margin: 1px;">
+                                                                        <label class="col-sm-4 col-form-label">DN No</label>
+                                                                        <div class="col-sm-6">
+                                                                            <select type="text" id="subcount_name_report" class="form-control select2" onchange="select_subcount(this.value)"></select>
+                                                                        </div>
+                                                                        <div class="col-sm-2">
+                                                                            <div class="checkbox-fade fade-in-primary">
+                                                                                <label>
+                                                                                    <input type="checkbox" value="" id="check_subcount" checked onchange="check_subcount()">
+                                                                                    <span class="cr">
+                                                                                        <i class="cr-icon ik ik-check txt-primary"></i>
+                                                                                    </span>
+                                                                                    <span>ALL</span>
+                                                                                </label>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="form-group row" style="margin: 1px;">
+                                                                        <label class="col-sm-4 col-form-label">SPK No</label>
+                                                                        <div class="col-sm-6">
+                                                                            <select type="text" id="spk_no_report" class="form-control select2" onchange="select_spk_no(this.value)"></select>
+                                                                        </div>
+                                                                        <div class="col-sm-2">
+                                                                            <div class="checkbox-fade fade-in-primary">
+                                                                                <label>
+                                                                                    <input type="checkbox" value="" id="check_spk" checked onchange="check_spk()">
+                                                                                    <span class="cr">
+                                                                                        <i class="cr-icon ik ik-check txt-primary"></i>
+                                                                                    </span>
+                                                                                    <span>ALL</span>
+                                                                                </label>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <br>
+                                                                    <div class="form-group row" style="margin: 1px;">
+                                                                        <label class="col-sm-4 col-form-label"></label>
+                                                                        <div class="col-sm-6">
+                                                                            <button type="button" id="filter_report" class="btn btn-primary" onclick="filter()">Filter</button>
+                                                                            <button type="button" id="clear_report"class="btn btn-secondary" onclick="clear()">Clear</button>
+                                                                            <button type="button" class="btn btn-info" id="print_report" onclick="print()">Print</button>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-md-6">
+                                                                <div class="col-md-12">
+                                                                    <div class="form-group row" style="margin: 1px;">
+                                                                        <label class="col-sm-4 col-form-label">LPP No</label>
+                                                                        <div class="col-sm-6">
+                                                                            <select type="text" id="subcount_name_report" class="form-control select2" onchange="select_subcount(this.value)"></select>
+                                                                        </div>
+                                                                        <div class="col-sm-2">
+                                                                            <div class="checkbox-fade fade-in-primary">
+                                                                                <label>
+                                                                                    <input type="checkbox" value="" id="check_subcount" checked onchange="check_subcount()">
+                                                                                    <span class="cr">
+                                                                                        <i class="cr-icon ik ik-check txt-primary"></i>
+                                                                                    </span>
+                                                                                    <span>ALL</span>
+                                                                                </label>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="form-group row" style="margin: 1px;">
+                                                                        <label class="col-sm-4 col-form-label">Item Code</label>
+                                                                        <div class="col-sm-6">
+                                                                            <select type="text" id="item_code_report" class="form-control select2" onchange="select_item_code(this.value)"></select>
+                                                                        </div>
+                                                                        <div class="col-sm-2">
+                                                                            <div class="checkbox-fade fade-in-primary">
+                                                                                <label>
+                                                                                    <input type="checkbox" value="" id="check_item" checked onchange="check_item()">
+                                                                                    <span class="cr">
+                                                                                        <i class="cr-icon ik ik-check txt-primary"></i>
+                                                                                    </span>
+                                                                                    <span>ALL</span>
+                                                                                </label>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="form-group row" style="margin: 1px;">
+                                                                        <label class="col-sm-4 col-form-label">Description</label>
+                                                                        <div id="spk_no" class="col-sm-8">
+                                                                            <input type="text" id="item_name_report" class="form-control"> 
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <br>
                                                         <div class="dt-responsive">
                                                             <table id="receiving_disubcount" class="table table-striped table-bordered nowrap">
                                                                 <thead>
